e33db17f235db3c6e9348138513d944f8977f3ef
Jakub Ziemba <ziemba.jak@gmail.com>
2022-08-25 12:29:56 +0200

    [FIX] Styling, fonts

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,22 @@ export default function Home() {
 	return (
 		<Main>
 			<Loader />
-			<Blobs></Blobs>
-			<Heading>coming soon</Heading>
+			<TextContainer>
+				<Heading>Kamila Palmowska</Heading>
+				<Subheading>interior design studio</Subheading>
+			</TextContainer>
+			<Footer>
+				<Paragraph>contact:</Paragraph>
+				<Paragraph>
+					<a href='tel:+48600897921'>600 897 921</a>
+				</Paragraph>
+				<Paragraph>
+					<a href='mailto:hello@kamilapalmowska.com'>hello@kamilapalmowska.com</a>
+				</Paragraph>
+			</Footer>
 		</Main>
 	);
 }
-
-const BlurInOut = keyframes`
-  from {
-    filter: blur(86px);
-  } to {
-    filter:blur(150px);
-  }
-`;
 
 const FadeOut = keyframes`
 	from {
@@ -30,50 +33,67 @@ export const Main = styled.main`
 	position: relative;
 	width: 100%;
 	height: 100vh;
-	background: #063525;
+	background: #8099d1;
 	overflow: hidden;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin: 0;
 `;
 
-export const Blobs = styled.div`
-	position: absolute;
-	top: -2%;
-	left: 0;
-	transform: translate(-50%, -50%);
-	width: 500vw;
-	height: 100vh;
-	background-color: #e0a458;
-	filter: blur(86px);
-	animation: ${BlurInOut} 5s alternate infinite;
-	z-index: 0;
-	border-radius: 50%;
-	opacity: 70%;
+export const TextContainer = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+	top: 50%;
+	transform: translateY(-50%);
 `;
 
-export const Blob = styled.div`
-	position: absolute;
-	background-color: ${props => props.color};
-	width: 20vw;
-	height: 30vw;
-	border-radius: 20px 100px 8px 69px;
-	margin-bottom: 20px;
-	z-index: 4;
-	top: ${props => props.top};
-	left: ${props => props.left};
-	/* transform: translate(-50%, -50%); */
+export const Footer = styled.footer`
+	margin-top: auto;
+	margin-bottom: 4rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	align-items: center;
+	position: relative;
+	bottom: 0;
 `;
 
 export const Heading = styled.h1`
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	z-index: 10;
-	font-variation-settings: 'wght' 420;
-	color: #dab1cf;
+	color: #f3dedb;
 	margin: 0;
 	white-space: nowrap;
 	font-size: 10vw;
 	letter-spacing: 0.01em;
+	font-weight: 300;
+	-webkit-font-smoothing: antialiased;
+	line-height: 0.65;
+`;
+
+export const Subheading = styled.h2`
+	font-size: 4vw;
+	color: #f3dedb;
+	font-weight: 300;
+	-webkit-font-smoothing: antialiased;
+`;
+
+export const Paragraph = styled.p`
+	font-size: 1rem;
+	color: #f3dedb;
+	font-weight: 300;
+	-webkit-font-smoothing: antialiased;
+	line-height: 1;
+	margin-bottom: 0;
+
+	@media (min-width: 600px) {
+		font-size: 1.5rem;
+	}
 `;
 
 export const Loader = styled.div`
@@ -86,4 +106,5 @@ export const Loader = styled.div`
 	opacity: 1;
 	z-index: 10000;
 	animation: ${FadeOut} 2s ease-out forwards;
+	pointer-events: none;
 `;
