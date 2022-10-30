445faefe13284f6bcfb82302ce6b5f944682fa3a
Jakub Ziemba <ziemba.jak@gmail.com>
2022-10-30 23:28:20 +0100

    [FIX] Layout styling

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,12 +1,13 @@
 import Nav from '../Nav';
 import Footer from '../Footer';
+import styles from './styles.module.scss';
 
 export default function Layout({ children }) {
 	return (
-		<>
+		<div className={styles.wrapper}>
 			<Nav />
-			<main>{children}</main>
+			<main className={styles.main}>{children}</main>
 			<Footer />
-		</>
+		</div>
 	);
 }
