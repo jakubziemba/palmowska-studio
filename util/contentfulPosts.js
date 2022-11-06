3bed4b1bcd4664916e83626fcff27cda7037be72
Jakub Ziemba <ziemba.jak@gmail.com>
2022-11-06 18:51:38 +0100

    [FIX] Comment out the code

--- util/contentfulPosts.js
+++ util/contentfulPosts.js
@@ -1,15 +1,15 @@
-import { createClient } from 'contentful';
+// import { createClient } from 'contentful';
 
-const space = process.env.CONTENTFUL_SPACE_ID;
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
+// const space = process.env.CONTENTFUL_SPACE_ID;
+// const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
 
-const client = createClient({
-	space: space,
-	accessToken: accessToken,
-});
+// const client = createClient({
+// 	space: space,
+// 	accessToken: accessToken,
+// });
 
-export default async function fetchEntries() {
-	const entries = await client.getEntries();
-	if (entries.items) return entries.items;
-	console.log(`Error getting Entries for ${contentType.name}.`);
-}
+// export default async function fetchEntries() {
+// 	const entries = await client.getEntries();
+// 	if (entries.items) return entries.items;
+// 	console.log(`Error getting Entries for ${contentType.name}.`);
+// }
