cf17ab934a1f9dd6fc41f83b1b709be09217014c
Jakub Ziemba <ziemba.jak@gmail.com>
2022-11-16 21:37:35 +0100

    [WIP] Add publications content

--- pages/publikacje/index.jsx
+++ pages/publikacje/index.jsx
@@ -1,5 +1,19 @@
 import styles from './styles.module.scss';
 
 export default function Publikacje() {
-	return <div>PUBLIKACJE</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.wrapper}>
+				<h1>Publikacje</h1>
+				<div className={styles.content}>
+					<div className={styles.publication}>Label Magazine</div>
+					{/* <div className={styles.listWrapper}></div> */}
+					<div className={styles.publication}>Elle Decoration</div>
+					<div className={styles.publication}>Whitemad Magazine</div>
+					<div className={styles.publication}>Design Alive</div>
+					<div className={styles.publication}>Internity Home</div>
+				</div>
+			</div>
+		</div>
+	);
 }
