101892a7561f7d34b89b3446e98972ab15bd72f7
Jakub Ziemba <ziemba.jak@gmail.com>
2022-12-16 15:31:09 +0100

    [FEAT] Add masonry package

--- pages/projekty/[slug].jsx
+++ pages/projekty/[slug].jsx
@@ -1,153 +1,159 @@
 import { createClient } from 'contentful';
 import Image from 'next/future/image';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import styles from './slug.module.scss';
 
 const client = createClient({
-	space: process.env.CONTENTFUL_SPACE_ID,
-	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
 
 export default function Project({ project }) {
-	const {
-		projectName,
-		apartment,
-		location,
-		livingArea,
-		projectBy,
-		date,
-		photos,
-		projectDescription,
-		thumbnail,
-		images,
-	} = project;
+  const {
+    projectName,
+    apartment,
+    location,
+    livingArea,
+    projectBy,
+    date,
+    photos,
+    projectDescription,
+    thumbnail,
+    images,
+  } = project;
 
-	return (
-		<section className={styles.container}>
-			<Image
-				src={`https:${thumbnail.url}`}
-				width={thumbnail.details.image.width}
-				height={thumbnail.details.image.height}
-				placeholder='blur'
-				blurDataURL={`https:${thumbnail.url}?fm=jpg&fl=progressive`}
-				alt='Cover image'
-				loading='eager'
-				quality={100}
-				priority
-			/>
-			<div className={styles.wrapper}>
-				<div className={styles.title}>
-					<h1 className={styles.heading}>{projectName}</h1>
-					{apartment && (
-						<>
-							<p className={styles.subheading}>{apartment}</p>
-						</>
-					)}
-				</div>
-				<div className={styles.content}>
-					{location && (
-						<>
-							<p className={styles.textTitle}>Lokalizacja</p>
-							<p className={styles.text}>{location}</p>
-						</>
-					)}
-					{livingArea && (
-						<>
-							<p className={styles.textTitle}>Metraż</p>
-							<p className={styles.text}>{livingArea}</p>
-						</>
-					)}
-					{projectBy && (
-						<>
-							<p className={styles.textTitle}>Projekt</p>
-							<p className={styles.text}>{projectBy}</p>
-						</>
-					)}
-					{date && (
-						<>
-							<p className={styles.textTitle}>Data realizacji</p>
-							<p className={styles.text}>{date}</p>
-						</>
-					)}
-					{photos && (
-						<>
-							<p className={styles.textTitle}>Zdjęcia</p>
-							<p className={styles.text}>{photos}</p>
-						</>
-					)}
-					{projectDescription && (
-						<div className={styles.text}>{documentToReactComponents(projectDescription)}</div>
-					)}
-				</div>
-			</div>
-			<div className={styles.projects}>
-				<div className={styles.projectsImages}>
-					{images.map((item, index) => {
-						const { file } = item.fields;
-						const width = file.details.image.width;
-						const height = file.details.image.height;
+  return (
+    <section className={styles.container}>
+      <Image
+        src={`https:${thumbnail.url}?fm=webp`}
+        width={thumbnail.details.image.width}
+        height={thumbnail.details.image.height}
+        placeholder='blur'
+        blurDataURL={`https:${thumbnail.url}?fm=jpg&fl=progressive`}
+        alt='Cover image'
+        loading='eager'
+        quality={100}
+        priority
+      />
+      <div className={styles.wrapper}>
+        <div className={styles.title}>
+          <h1 className={styles.heading}>{projectName}</h1>
+          {apartment && (
+            <>
+              <p className={styles.subheading}>{apartment}</p>
+            </>
+          )}
+        </div>
+        <div className={styles.content}>
+          {location && (
+            <>
+              <p className={styles.textTitle}>Lokalizacja</p>
+              <p className={styles.text}>{location}</p>
+            </>
+          )}
+          {livingArea && (
+            <>
+              <p className={styles.textTitle}>Metraż</p>
+              <p className={styles.text}>{livingArea}</p>
+            </>
+          )}
+          {projectBy && (
+            <>
+              <p className={styles.textTitle}>Projekt</p>
+              <p className={styles.text}>{projectBy}</p>
+            </>
+          )}
+          {date && (
+            <>
+              <p className={styles.textTitle}>Data realizacji</p>
+              <p className={styles.text}>{date}</p>
+            </>
+          )}
+          {photos && (
+            <>
+              <p className={styles.textTitle}>Zdjęcia</p>
+              <p className={styles.text}>{photos}</p>
+            </>
+          )}
+          {projectDescription && (
+            <div className={styles.text}>{documentToReactComponents(projectDescription)}</div>
+          )}
+        </div>
+      </div>
+      <div className={styles.projects}>
+        {/* <div className={styles.projectsImages}> */}
+        <ResponsiveMasonry columnsCountBreakPoints={{ 320: 1, 750: 2, 900: 3 }}>
+          <Masonry gutter='16px'>
+            {images.map(item => {
+              const { file } = item.fields;
+              const width = file.details.image.width;
+              const height = file.details.image.height;
 
-						return (
-							<div
-								key={index}
-								className={styles.imageWrapper}
-								style={{ gridColumn: width > height ? '1 / 3' : 'auto' }}
-							>
-								<Image
-									src={`https:${file.url}`}
-									width={width}
-									height={height}
-									alt={file.fileName}
-									quality={85}
-									placeholder='blur'
-									blurDataURL={`https:${file.url}?fm=jpg&fl=progressive`}
-								/>
-							</div>
-						);
-					})}
-				</div>
-			</div>
-		</section>
-	);
+              return (
+                // <div
+                // className={styles.imageWrapper}
+                // style={{ gridColumn: width > height ? '1 / 3' : 'auto' }}
+                // >
+                <Image
+                  key={file.fileName}
+                  src={`https:${file.url}`}
+                  width={width}
+                  height={height}
+                  alt={file.fileName}
+                  layout='responsive'
+                  quality={85}
+                  placeholder='blur'
+                  blurDataURL={`https:${file.url}?fm=jpg&fl=progressive`}
+                />
+                // </div>
+              );
+            })}
+          </Masonry>
+        </ResponsiveMasonry>
+        {/* </div> */}
+      </div>
+    </section>
+  );
 }
 
 export async function getStaticPaths() {
-	const res = await client.getEntries({ content_type: 'project' });
+  const res = await client.getEntries({ content_type: 'project' });
 
-	const paths = res.items.map(item => {
-		return {
-			params: { slug: item.fields.slug },
-		};
-	});
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
 
-	return {
-		paths,
-		fallback: false,
-	};
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-	const { items } = await client.getEntries({
-		content_type: 'project',
-		'fields.slug': params.slug,
-	});
+  const { items } = await client.getEntries({
+    content_type: 'project',
+    'fields.slug': params.slug,
+  });
 
-	const projectData = items.map(({ fields }) => {
-		return {
-			id: fields?.id,
-			projectName: fields?.projectName,
-			apartment: fields?.apartment,
-			location: fields?.location,
-			livingArea: fields?.livingArea,
-			projectBy: fields?.projectBy,
-			date: fields?.date,
-			photos: fields?.photos,
-			thumbnail: fields?.thumbnail?.fields?.file,
-			images: fields?.images,
-		};
-	});
+  const projectData = items.map(({ fields }) => {
+    return {
+      id: fields?.id,
+      projectName: fields?.projectName,
+      apartment: fields?.apartment,
+      location: fields?.location,
+      livingArea: fields?.livingArea,
+      projectBy: fields?.projectBy,
+      date: fields?.date,
+      photos: fields?.photos,
+      thumbnail: fields?.thumbnail?.fields?.file,
+      images: fields?.images,
+    };
+  });
 
-	return {
-		props: { project: projectData[0] },
-	};
+  return {
+    props: { project: projectData[0] },
+  };
 }
