995d41e2729d5949b7af59ef57566f45ea58c004
Jakub Ziemba <ziemba.jak@gmail.com>
2023-08-13 17:03:37 +0200

    [FIX] Fix Next config for image optimization

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     domains: ['images.ctfassets.net'],
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+      },
+    ],
   },
 
   webpack(config) {
