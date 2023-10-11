9fd0e5b137f639f40fce1b77b68a1e9fa50ca559
Jakub Ziemba <ziemba.jak@gmail.com>
2023-10-11 20:05:34 +0200

    [FIX] Footer update

--- app/components/Footer/index.jsx
+++ app/components/Footer/index.jsx
@@ -2,6 +2,7 @@
 
 import Logo from "../../../public/palmowska-logo-bw.svg";
 import Link from "next/link";
+import { FaPinterest, FaFacebook, FaInstagram } from "react-icons/fa";
 
 const routes = [
   { path: "/projekty", label: "Projekty" },
@@ -12,11 +13,14 @@ const routes = [
 ];
 
 const socialMediaLinks = [
-  { path: "https://www.instagram.com/palmowska__studio", label: "Instagram" },
-  { path: "https://www.pinterest.com/kpalmowska", label: "Pinterest" },
+  {
+    path: "https://www.instagram.com/palmowska__studio",
+    label: <FaInstagram />,
+  },
+  { path: "https://www.pinterest.com/kpalmowska", label: <FaPinterest /> },
   {
     path: "https://www.facebook.com/profile.php?id=100087842247172",
-    label: "Facebook",
+    label: <FaFacebook />,
   },
 ];
 
@@ -25,41 +29,41 @@ export default function Footer() {
     <footer className="flex w-full flex-col bg-lightblue px-page-mobile py-8 lg:px-page-desktop">
       <div className="flex flex-col gap-10">
         <Link className="w-max" href="/">
-          <Logo className="h-[88px] w-16" />
+          <Logo className="h-20 w-16" />
         </Link>
+      </div>
+      <div className="my-8 grid auto-rows-auto grid-cols-[1.5fr,1fr] gap-4 gap-y-8">
         <h5 className="font-sans text-lg font-semibold uppercase">
           Palmowska Studio
         </h5>
-      </div>
-      <div className="my-8 mb-14 grid grid-cols-2">
-        <div className="flex flex-col gap-3 text-md">
+        <div className="row-start-1 row-end-3 flex flex-col gap-3 text-md">
           {routes.map((link) => (
             <Link key={link.path} href={link.path} className="w-max font-sans">
               {link.label}
             </Link>
           ))}
         </div>
-        <div className="flex w-full flex-col gap-3 font-sans text-md font-medium">
+        <div className="col-start-1 row-start-2 flex flex-col justify-center font-sans text-base font-medium">
+          <a
+            className="font-sans text-base"
+            href="mailto:hello@kamilapalmowska.com"
+          >
+            hello@kamilapalmowska.com
+          </a>
+          <a className="font-sans text-base" href="tel:+48 600 897 821">
+            +48 600 897 821
+          </a>
+        </div>
+        <div className="flex w-full flex-row items-end gap-3 font-sans text-md font-medium">
           {socialMediaLinks.map((link) => (
             <Link key={link.path} href={link.path} className="w-max">
               {link.label}
             </Link>
           ))}
         </div>
-      </div>
-      <div className="flex flex-col justify-center font-sans text-base font-medium">
-        <a
-          className="font-sans text-md"
-          href="mailto:hello@kamilapalmowska.com"
-        >
-          hello@kamilapalmowska.com
-        </a>
-        <a className="font-sans text-md" href="tel:+48 600 897 821">
-          +48 600 897 821
-        </a>
-      </div>
-      <div className="mt-8 font-sans text-sm">
-        © 2023 - All rights reserved.
+        <div className="mt-8 font-sans text-sm">
+          © 2023 - All rights reserved.
+        </div>
       </div>
     </footer>
   );
